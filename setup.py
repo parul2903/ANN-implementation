7e066da813feb84e911e1791516cbc0c8909148a
Parul Sharma <parul3kin@.com>
2021-11-28 19:53:56 +0530

    save_model function added

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/parul2903/ANN-implementation",
     packages = ["src"],
-    python_requires = ">=3.9",
+    python_requires = ">=3.8",
     install_requires = [
         "tensorflow",
         "matplotlib",
--- src/training.py
+++ src/training.py
@@ -1,13 +1,15 @@
 from src.utils.common_utils import read_config
-from src.utils.data_management import get_data 
-from src.utils.model import create_model
+from src.utils.data_management import get_data
+from src.utils.model import create_model, save_model
 import argparse
+import os
+
 
 def training(config_path):
     config = read_config(config_path)
 
-    validation_datasize = config['params']['validation_data_size']
-    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data(validation_datasize)
+    validation_data_size = config['params']['validation_data_size']
+    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data(validation_data_size)
 
     LOSS_FUNCTION = config['params']['loss_function']
     OPTIMIZER = config['params']['optimizer']
@@ -18,13 +20,22 @@ def training(config_path):
     EPOCHS = config['params']['epochs']
     VALIDATION = (x_valid, y_valid)
 
-    model_fit = model.fit(x_train, y_train, epochs = EPOCHS, validation_data = VALIDATION)
+    model_fit = model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    model_name = config['artifacts']['model_name']
+    model_dir = config['artifacts']['model_dir']
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    save_model(model, model_name, model_dir_path)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # we can either use "--config" or "-c"
-    parser.add_argument("--config", "-c", default = "config.yaml", help = "to read the config file")
+    parser.add_argument("--config", "-c", default="config.yaml", help="to read the config file")
 
     parsed_args = parser.parse_args()
 
-    training(config_path = parsed_args.config) 
+    training(config_path=parsed_args.config)
